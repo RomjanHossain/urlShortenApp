37c0575bac66499a3382bab59b85e2c52d0843d8
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-10 23:58:56 +0600

    splash screen

---
+++ lib/presentation/pages/splash/bloc/splash_event.dart
@@ -0,0 +1,19 @@
+part of 'splash_bloc.dart';
+
+abstract class SplashEvent extends Equatable {
+  const SplashEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+/// {@template custom_splash_event}
+/// Event added when some custom logic happens
+/// {@endtemplate}
+class InitSplashEvent extends SplashEvent {
+  /// {@macro custom_splash_event}
+  const InitSplashEvent(this.context);
+
+  /// context
+  final BuildContext context;
+}
