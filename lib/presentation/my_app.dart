913810afdef5a6fd0b9a0f87d613ac21a04247a1
RomjanHossain <romjan@skiff.com>
2023-07-11 12:24:49 +0600

    implement hpp

--- lib/presentation/my_app.dart
+++ lib/presentation/my_app.dart
@@ -1,7 +1,7 @@
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:url_shorten/core/app_core.dart';
+import 'package:url_shorten/core/params/home_page_params.dart';
 import 'pages/home/bloc/home_bloc.dart';
 import 'pages/splash/splash.dart';
 
@@ -12,15 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final isPlatformDark =
         WidgetsBinding.instance.window.platformBrightness == Brightness.dark;
-    final initTheme = isPlatformDark
-        ? FlexThemeData.dark(
-            scheme: FlexScheme.indigoM3,
-            useMaterial3: true,
-          )
-        : FlexThemeData.light(
-            scheme: FlexScheme.indigoM3,
-            useMaterial3: true,
-          );
+    final initTheme =
+        isPlatformDark ? HomePageParams.darkTheme : HomePageParams.lightTheme;
     return MultiBlocProvider(
       providers: [
         BlocProvider<HomeBloc>(
@@ -29,25 +22,18 @@ class MyApp extends StatelessWidget {
       ],
       // create: (context) => SubjectBloc(),
       child: ThemeProvider(
-          initTheme: initTheme,
-          builder: (_, theme) {
-            return MaterialApp(
-              title: Strings.title,
-              // theme: ThemeData(
-              //   colorScheme: ColorScheme.fromSeed(seedColor: Strings.primaryColor),
-              //   useMaterial3: true,
-              // ),
-              debugShowCheckedModeBanner: false,
-              theme: theme,
-              // darkTheme: FlexThemeData.light(
-              //   scheme: FlexScheme.indigoM3,
-              //   useMaterial3: true,
-              // ),
-              // themeMode: ThemeMode.system,
-              home: const SplashPage(),
-              // home: const HomePage(),
-            );
-          }),
+        initTheme: initTheme,
+        builder: (_, theme) {
+          return MaterialApp(
+            title: Strings.title,
+
+            debugShowCheckedModeBanner: false,
+            theme: theme,
+            home: const SplashPage(),
+            // home: const HomePage(),
+          );
+        },
+      ),
     );
   }
 }
