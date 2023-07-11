e4dd16a7603d129f27d47cba48f595ad756d6844
RomjanHossain <romjan@skiff.com>
2023-07-11 17:03:45 +0600

    checking if the previous url is same or not

--- lib/presentation/pages/viewshorturl/bloc/viewshorturl_state.dart
+++ lib/presentation/pages/viewshorturl/bloc/viewshorturl_state.dart
@@ -1,8 +1,7 @@
 part of 'viewshorturl_bloc.dart';
 
-/// {@template viewshorturl_state}
 /// ViewshorturlState description
-/// {@endtemplate}
+
 class ViewshorturlState extends Equatable {
   /// {@macro viewshorturl_state}
   const ViewshorturlState();
@@ -11,13 +10,14 @@ class ViewshorturlState extends Equatable {
   List<Object> get props => [];
 }
 
-/// {@template viewshorturl_initial}
 /// The initial state of ViewshorturlState
-/// {@endtemplate}
+
 class ViewshorturlInitial extends ViewshorturlState {
   /// {@macro viewshorturl_initial}
   const ViewshorturlInitial(this.url) : super();
 
   /// main url
   final String url;
+  @override
+  List<Object> get props => [url];
 }
