c3e4859f0a8254a4394f3ac1220e051503c35538
RomjanHossain <romjan@skiff.com>
2023-07-11 12:24:30 +0600

    home page parmas

--- lib/core/params/home_page_params.dart
+++ lib/core/params/home_page_params.dart
@@ -1,1 +1,19 @@
-const double bottomNavBorderRadius = 15;
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+
+class HomePageParams {
+  /// Bottom Navigation radius
+  static const double bottomNavBorderRadius = 15;
+
+  /// dark colorshceme
+  static ThemeData darkTheme = FlexThemeData.dark(
+    scheme: FlexScheme.orangeM3,
+    useMaterial3: true,
+  );
+
+  /// light colorshceme
+  static ThemeData lightTheme = FlexThemeData.light(
+    scheme: FlexScheme.orangeM3,
+    useMaterial3: true,
+  );
+}
