2a33ae913e7d6f9dccee25f306fbf96b102b7388
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-11 12:25:53 +0600

    home page minimize

--- lib/presentation/pages/home/view/home_page.dart
+++ lib/presentation/pages/home/view/home_page.dart
@@ -1,9 +1,8 @@
-import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
-import 'package:url_shorten/presentation/pages/home/bloc/bloc.dart';
 import 'package:url_shorten/presentation/pages/home/widgets/home_body.dart';
+import 'package:url_shorten/presentation/pages/home/widgets/home_fab.dart';
+import 'package:url_shorten/presentation/pages/home/widgets/home_navbar.dart';
 
 /// {@template home_page}
 /// A description for HomePage
@@ -15,59 +14,19 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ThemeSwitchingArea(
-      child: Scaffold(
+      child: const Scaffold(
         extendBody: true,
-        body: const HomeView(),
-        floatingActionButton: ThemeSwitcher.withTheme(
-          builder: (_, switcher, theme) {
-            return FloatingActionButton(
-              shape: const CircleBorder(),
-              onPressed: () => switcher.changeTheme(
-                theme: theme.brightness == Brightness.light
-                    ? FlexThemeData.dark(
-                        scheme: FlexScheme.indigoM3,
-                        useMaterial3: true,
-                      )
-                    : FlexThemeData.light(
-                        scheme: FlexScheme.indigoM3,
-                        useMaterial3: true,
-                      ),
-              ),
-            );
-          },
-        ),
+        body: HomeView(),
+        floatingActionButton: HomeFloatingActionBtn(),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: AnimatedBottomNavigationBar(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          activeIndex: context.watch<HomeBloc>().state is HomeInitial
-              ? 0
-              : context.watch<HomeBloc>().state is SettingPageState
-                  ? 1
-                  : 2,
-          gapLocation: GapLocation.center,
-          notchSmoothness: NotchSmoothness.verySmoothEdge,
-          leftCornerRadius: 10,
-          rightCornerRadius: 10,
-          // splashSpeedInMilliseconds: 800,
-
-          icons: [
-            Icons.home,
-            Icons.settings,
-            Icons.history,
-            Icons.history,
-          ],
-          onTap: (x) {
-            context.read<HomeBloc>().add(ChangePageEvent(x));
-          },
-        ),
+        bottomNavigationBar: HomeBottomNav(),
       ),
     );
   }
 }
 
-/// {@template home_view}
 /// Displays the Body of HomeView
-/// {@endtemplate}
+
 class HomeView extends StatelessWidget {
   /// {@macro home_view}
   const HomeView({super.key});
