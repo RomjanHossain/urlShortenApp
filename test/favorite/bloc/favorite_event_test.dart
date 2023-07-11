4878ebec6cf774d5857f1fbbc9725cbe1de0a7f3
RomjanHossain <romjan@skiff.com>
2023-07-11 12:39:46 +0600

    test cases for fav, his and settings

---
+++ test/favorite/bloc/favorite_event_test.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:url_shorten/presentation/pages/favorite/bloc/bloc.dart';
+
+void main() {
+  group('FavoriteEvent', () {  
+    group('CustomFavoriteEvent', () {
+      test('supports value equality', () {
+        expect(
+          CustomFavoriteEvent(),
+          equals(const CustomFavoriteEvent()),
+        );
+      });
+    });
+    group('constructor', () {
+      test('can be instantiated', () {
+        expect(
+          const CustomFavoriteEvent(),
+          isNotNull
+        );
+      });
+    });
+  });
+}
