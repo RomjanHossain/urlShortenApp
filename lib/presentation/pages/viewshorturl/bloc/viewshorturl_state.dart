47c343a4e6b74e0e8823c0f7d4ac511f44bb858d
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-12 12:00:51 +0600

    view states

--- lib/presentation/pages/viewshorturl/bloc/viewshorturl_state.dart
+++ lib/presentation/pages/viewshorturl/bloc/viewshorturl_state.dart
@@ -21,3 +21,65 @@ class ViewshorturlInitial extends ViewshorturlState {
   @override
   List<Object> get props => [url];
 }
+
+/// loading state (free)
+class ViewshorturlLoading extends ViewshorturlState {
+  /// {@macro viewshorturl_loading}
+  const ViewshorturlLoading() : super();
+}
+
+/// loading state (premium)
+class ViewshorturlPremiumLoading extends ViewshorturlState {
+  /// {@macro viewshorturl_loading}
+  const ViewshorturlPremiumLoading() : super();
+}
+
+/// error state (free)
+class ViewshorturlError extends ViewshorturlState {
+  /// {@macro viewshorturl_error}
+  const ViewshorturlError(this.message) : super();
+
+  /// error message
+  final String message;
+  @override
+  List<Object> get props => [message];
+}
+
+/// error state (premium)
+class ViewshorturlPremiumError extends ViewshorturlState {
+  /// {@macro viewshorturl_error}
+  const ViewshorturlPremiumError(this.message) : super();
+
+  /// error message
+  final String message;
+  @override
+  List<Object> get props => [message];
+}
+
+/// No Internet
+class ConnectionError extends ViewshorturlState {
+  /// {@macro viewshorturl_loading}
+  const ConnectionError() : super();
+}
+
+/// success state (free)
+class ViewshorturlSuccess extends ViewshorturlState {
+  /// {@macro viewshorturl_error}
+  const ViewshorturlSuccess(this.surls) : super();
+
+  /// error short urls
+  final List<Object> surls;
+  @override
+  List<Object> get props => [surls];
+}
+
+/// sucess state (premium)
+class ViewshorturlPremiumSuccess extends ViewshorturlState {
+  /// {@macro viewshorturl_error}
+  const ViewshorturlPremiumSuccess(this.surls) : super();
+
+  /// error short urls
+  final List<Object> surls;
+  @override
+  List<Object> get props => [surls];
+}
