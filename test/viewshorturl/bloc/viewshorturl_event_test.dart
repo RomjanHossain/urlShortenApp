08d16697723792e35abf4aca72a6c0f89673303a
RomjanHossain <romjan@skiff.com>
2023-07-12 11:59:55 +0600

    test viewshorturl

--- test/viewshorturl/bloc/viewshorturl_event_test.dart
+++ test/viewshorturl/bloc/viewshorturl_event_test.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:url_shorten/presentation/pages/viewshorturl/bloc/bloc.dart';
 
 void main() {
   group('ViewshorturlEvent', () {  
