2b9669b0bc238eff7b2c726a90b3c2db03fdc739
RomjanHossain <romjan@skiff.com>
2023-07-13 18:46:39 +0600

    rebrandly repo

---
+++ lib/domain/repositories/rebrandly_repo.dart
@@ -0,0 +1,8 @@
+import 'package:url_shorten/core/error/api_errors.dart';
+import 'package:url_shorten/domain/entities/rebrandly_entities.dart';
+
+/// rebradly repo
+abstract class RebrandlyRepository {
+  Future<Result<RebrandlyEntities, Exception>> shortUrl(
+      String url, String custom);
+}
