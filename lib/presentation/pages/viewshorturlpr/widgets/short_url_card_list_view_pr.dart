381a4532e283f13445ae6fd79419ce7d690dfc7a
RomjanHossain <romjan@skiff.com>
2023-07-13 18:48:40 +0600

    premium views on widget

--- lib/presentation/pages/viewshorturlpr/widgets/short_url_card_list_view_pr.dart
+++ lib/presentation/pages/viewshorturlpr/widgets/short_url_card_list_view_pr.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:url_shorten/domain/entities/error_entities.dart';
+import 'package:url_shorten/domain/entities/rebrandly_entities.dart';
+import 'package:url_shorten/domain/entities/tinyurl_enitites.dart';
 import 'package:url_shorten/domain/entities/ulvis_entities.dart';
+import 'package:url_shorten/domain/entities/urlbae_entities.dart';
 import 'package:url_shorten/presentation/pages/viewshorturl/widgets/shrt_small_card.dart';
 import 'package:url_shorten/presentation/pages/viewshorturlpr/bloc/viewshorturlpr_bloc.dart';
 
@@ -22,12 +26,54 @@ class ShortUrlCardListViewPr extends StatelessWidget {
         }
         Object data = state.surls[index];
 
+        print('$index ${data.runtimeType}');
         if (data.runtimeType == UlvisEntity) {
           data = data as UlvisEntity;
           return Card(
             child: ListTile(
               title: const Text('Ulvis'),
               subtitle: ShrtcoSmllCard(txt: data.data.url),
+            ),
+          );
+        }
+        if (data.runtimeType == RebrandlyEntities) {
+          data = data as RebrandlyEntities;
+          return Card(
+            child: ListTile(
+              title: const Text('Rebrandly'),
+              subtitle: ShrtcoSmllCard(txt: data.shortURL),
+            ),
+          );
+        }
+        if (data.runtimeType == TinyUrlEntities) {
+          data = data as TinyUrlEntities;
+
+          return Card(
+            child: ListTile(
+              title: const Text('TinyUrl'),
+              subtitle: ShrtcoSmllCard(txt: data.data?.url ?? ""),
+            ),
+          );
+        }
+        if (data.runtimeType == ErrorModel) {
+          data = data as ErrorModel;
+
+          return Card(
+            child: ListTile(
+              title: Text('Errors ${data.domain}'),
+              subtitle: ShrtcoSmllCard(txt: data.message),
+            ),
+          );
+        }
+        if (data.runtimeType == UrlBaeEntity) {
+          data = data as UrlBaeEntity;
+// ErrorModel
+          return Card(
+            child: ListTile(
+              title: const Text('UrlBae'),
+              subtitle: ShrtcoSmllCard(
+                txt: data.shortUrl ?? 'Empty',
+              ),
             ),
           );
         }
