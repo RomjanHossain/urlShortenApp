464246cc3a8a26bf9c40c4e274440b423fcaa605
RomjanHossain <romjan@skiff.com>
2023-07-13 06:39:46 +0600

    loading listview

---
+++ lib/presentation/pages/viewshorturl/widgets/loading_listview.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+class LoadingListView extends StatelessWidget {
+  const LoadingListView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 4,
+      itemBuilder: (context, index) {
+        return SizedBox(
+          height: 200,
+          child: const Card(
+            margin: EdgeInsets.all(8),
+            child: ListTile(
+              title: Text(''),
+              subtitle: Text(''),
+            ),
+          )
+              .animate(
+                onPlay: (controller) => controller.repeat(),
+              )
+              .shimmer(
+                delay: 500.milliseconds,
+                angle: 2,
+                color: Theme.of(context).colorScheme.secondary,
+                curve: Curves.easeIn,
+                duration: 800.milliseconds,
+              ),
+        );
+      },
+    );
+  }
+}
