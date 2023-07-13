4a62162cd58b18a981c5e1b96d3635bc8e5c5cb4
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-13 15:48:10 +0600

    removed original link

--- lib/presentation/pages/viewshorturl/widgets/shrt_card.dart
+++ lib/presentation/pages/viewshorturl/widgets/shrt_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_shorten/domain/entities/shrtco_entities.dart';
-import 'package:url_shorten/presentation/pages/viewshorturl/widgets/copy_icon_button.dart';
 import 'package:url_shorten/presentation/pages/viewshorturl/widgets/shrt_small_card.dart';
 
 class ShrtCoCard extends StatelessWidget {
@@ -23,33 +22,6 @@ class ShrtCoCard extends StatelessWidget {
             ShrtcoSmllCard(txt: shrtcoEntity.shortLink),
             ShrtcoSmllCard(txt: shrtcoEntity.shortLink2),
             ShrtcoSmllCard(txt: shrtcoEntity.shortLink3),
-
-            // original link
-            const Text(
-              'Original Link:',
-              textAlign: TextAlign.left,
-            ),
-            Card(
-              color: Theme.of(context).colorScheme.tertiary,
-              child: Stack(
-                children: [
-                  ListTile(
-                    subtitle: Text(
-                      shrtcoEntity.originalLink,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: CopyIconBtn(
-                        txt: shrtcoEntity.originalLink,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
           ],
         ),
       ),
