daa4814867a6dae588c0c1353c759c7bd7046053
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-13 18:46:24 +0600

    rebrandly repo impl

---
+++ lib/data/repositories/rebrandly_repo_impl.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:url_shorten/core/error/api_errors.dart';
+import 'package:url_shorten/core/resources/pr_resources.dart';
+import 'package:url_shorten/domain/entities/rebrandly_entities.dart';
+import 'package:url_shorten/domain/repositories/rebrandly_repo.dart';
+
+import '../../core/params/api_keys.dart';
+
+class RebrandlyRepoImp extends RebrandlyRepository {
+  @override
+  Future<Result<RebrandlyEntities, Exception>> shortUrl(
+      String url, String custom) async {
+    const String rebrandLy = PremiumApiResources.rebrandly;
+    try {
+      final Uri uri = Uri.parse('$rebrandLy?destination=$url&slashtag=$custom');
+
+      // final http.Response response = await http.post(uri, body: {'url': url});
+      /// a post method where the url is passed as a body and header will be content-type application/json
+      final http.Response response = await http.post(
+        uri,
+        headers: {
+          'apiKey': ApiKeys.rebrandlyToken,
+        },
+      );
+
+      // check if the response is successful
+      if (response.statusCode == 200) {
+        final result = json.decode(response.body);
+        final rebrandlyEntity = RebrandlyEntities.fromJson(result);
+        return Success(rebrandlyEntity);
+      } else {
+        // print('oh no! 400 -> ${response.statusCode}\n${response.body}');
+        return ServerFailor(Exception('Unable to get short url'));
+      }
+    } on Exception catch (e) {
+      return ServerFailor(e);
+    }
+  }
+}
