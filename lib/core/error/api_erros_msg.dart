ea0a537cdaada9c774c094c50389aed63e69a9b7
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-13 15:40:24 +0600

    alias already exists

--- lib/core/error/api_erros_msg.dart
+++ lib/core/error/api_erros_msg.dart
@@ -7,5 +7,8 @@ const String rateLimitMsg = 'Rate Limited By the Server';
 /// ip blocked
 const String ipBlockMsg = 'Your IP has been blocked by the Server';
 
+/// alias already exist
+const String aliasExistMsg = 'That alias is taken. Please choose another one.';
+
 /// unknown error
 const String unknownErrorMsg = 'Unknown error';
