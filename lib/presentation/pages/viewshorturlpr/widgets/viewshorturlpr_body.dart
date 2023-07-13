e98b6a18c64b5330658ebc38f1d4494469ea7996
RomjanHossain <romjan@skiff.com>
2023-07-13 15:46:25 +0600

    premium views

---
+++ lib/presentation/pages/viewshorturlpr/widgets/viewshorturlpr_body.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:url_shorten/presentation/pages/viewshorturlpr/widgets/short_url_card_list_view_pr.dart';
+import 'package:url_shorten/presentation/widgets/loading_listview.dart';
+import 'package:url_shorten/presentation/pages/viewshorturlpr/bloc/bloc.dart';
+
+/// {@template viewshorturlpr_body}
+/// Body of the ViewshorturlprPage.
+///
+/// Add what it does
+/// {@endtemplate}
+class ViewshorturlprBody extends StatelessWidget {
+  /// {@macro viewshorturlpr_body}
+  const ViewshorturlprBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ViewshorturlprBloc, ViewshorturlprState>(
+      builder: (context, state) {
+        if (state is ViewshorturlPremiumLoading) {
+          // return const Center(child: CircularProgressIndicator());
+          return const LoadingListView();
+        }
+        if (state is ViewshorturlPremiumError) {
+          return Center(
+            child: Text(
+              state.message,
+              textAlign: TextAlign.center,
+            ),
+          );
+        }
+        if (state is ViewshorturlPremiumSuccess) {
+          return ShortUrlCardListViewPr(state: state);
+        }
+        if (state is ConnectionError) {
+          return const Center(
+            child: Text(
+              'No Internet Connection',
+              textAlign: TextAlign.center,
+            ),
+          );
+        }
+        return const Center(child: Text('Premium Page'));
+      },
+    );
+  }
+}
