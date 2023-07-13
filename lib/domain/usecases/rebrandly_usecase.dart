541cc8749eb1ecc62ddb3bdd6499bfd8870b8885
RomjanHossain <romjan@skiff.com>
2023-07-13 18:47:03 +0600

    rebrandly usecases

---
+++ lib/domain/usecases/rebrandly_usecase.dart
@@ -0,0 +1,17 @@
+import 'package:url_shorten/core/error/api_errors.dart';
+import 'package:url_shorten/data/repositories/rebrandly_repo_impl.dart';
+import 'package:url_shorten/domain/entities/rebrandly_entities.dart';
+
+class RebrandlyUseCase {
+  RebrandlyRepoImp repository = RebrandlyRepoImp();
+
+  /// get shorted url or erros
+  Future<Result<RebrandlyEntities, Exception>> getShortUrl(
+      String url, String custom) async {
+    /*
+    !!!If theres any business logic that needs to be done 
+    !!!before returning the data, it should be done here
+    */
+    return await repository.shortUrl(url, custom);
+  }
+}
