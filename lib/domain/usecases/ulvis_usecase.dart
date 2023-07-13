98c6ff909683899f7d8d8a577d805107955bf2b5
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-13 15:42:42 +0600

    urlvis pr usecase

--- lib/domain/usecases/ulvis_usecase.dart
+++ lib/domain/usecases/ulvis_usecase.dart
@@ -3,13 +3,24 @@ import 'package:url_shorten/data/repositories/ulvis_repo_impl.dart';
 import 'package:url_shorten/domain/entities/ulvis_entities.dart';
 
 class UlvisUseCase {
+  UlvisRepoImpl repository = UlvisRepoImpl();
+
   /// get list of free short url or erros
   Future<Result<UlvisEntity, Exception>> getShortUrl(String url) async {
-    UlvisRepoImpl repository = UlvisRepoImpl();
     /*
     !!!If theres any business logic that needs to be done 
     !!!before returning the data, it should be done here
     */
     return await repository.shortUrl(url);
+  }
+
+  /// get list of premium short url or erros
+  Future<Result<UlvisEntity, Exception>> getShortPremiumUrl(
+      String url, String custom) async {
+    /*
+    !!!If theres any business logic that needs to be done 
+    !!!before returning the data, it should be done here
+    */
+    return await repository.prShortUrl(url, custom);
   }
 }
