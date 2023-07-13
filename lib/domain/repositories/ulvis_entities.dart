7e98dcae91fc2b35653490338a836718ae1093a4
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-13 06:38:02 +0600

    ulvis entities

---
+++ lib/domain/repositories/ulvis_entities.dart
@@ -0,0 +1,7 @@
+import 'package:url_shorten/core/error/api_errors.dart';
+import 'package:url_shorten/domain/entities/ulvis_entities.dart';
+
+/// ShortUrlRepository
+abstract class UlvisRepository {
+  Future<Result<UlvisEntity, Exception>> shortUrl(String url);
+}
