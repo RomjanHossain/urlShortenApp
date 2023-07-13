ca12a6f20a4a158214c4c07a44d015543ff129fe
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-13 06:39:13 +0600

    extracts widgets

--- lib/presentation/pages/viewshorturl/widgets/viewshorturl_body.dart
+++ lib/presentation/pages/viewshorturl/widgets/viewshorturl_body.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:url_shorten/domain/entities/cleanuri_entities.dart';
-import 'package:url_shorten/domain/entities/error_entities.dart';
-import 'package:url_shorten/domain/entities/gotiny_entities.dart';
-import 'package:url_shorten/domain/entities/shrtco_entities.dart';
 import 'package:url_shorten/presentation/pages/viewshorturl/bloc/bloc.dart';
-import 'package:url_shorten/presentation/pages/viewshorturl/widgets/shrt_card.dart';
-import 'package:url_shorten/presentation/pages/viewshorturl/widgets/shrt_small_card.dart';
+import 'package:url_shorten/presentation/pages/viewshorturl/widgets/loading_listview.dart';
+import 'package:url_shorten/presentation/pages/viewshorturl/widgets/shorturl_card_listview.dart';
 
 /// {@template viewshorturl_body}
 /// Body of the ViewshorturlPage.
@@ -30,82 +25,10 @@ class ViewshorturlBody extends StatelessWidget {
         }
         if (state is ViewshorturlLoading) {
           // return const Center(child: CircularProgressIndicator());
-          return ListView.builder(
-            itemCount: 3,
-            itemBuilder: (context, index) {
-              return SizedBox(
-                height: 200,
-                child: const Card(
-                  margin: EdgeInsets.all(8),
-                  child: ListTile(
-                    title: Text(''),
-                    subtitle: Text(''),
-                  ),
-                )
-                    .animate(
-                      onPlay: (controller) => controller.repeat(),
-                    )
-                    .shimmer(
-                      delay: 500.milliseconds,
-                      angle: 2,
-                      color: Theme.of(context).colorScheme.secondary,
-                      curve: Curves.easeIn,
-                      duration: 800.milliseconds,
-                    ),
-              );
-            },
-          );
+          return const LoadingListView();
         }
         if (state is ViewshorturlSuccess) {
-          return ListView.builder(
-            itemCount: state.surls.length,
-            itemBuilder: (context, index) {
-              Object _data = state.surls[index];
-              print(_data.runtimeType);
-              if (_data.runtimeType == ShrtcoEntity) {
-                _data = _data as ShrtcoEntity;
-
-                return ShrtCoCard(
-                  shrtcoEntity: _data,
-                );
-              }
-              if (_data.runtimeType == CleanUriEntities) {
-                _data = _data as CleanUriEntities;
-                return Card(
-                  child: ListTile(
-                    title: const Text('CleanUri'),
-                    subtitle: ShrtcoSmllCard(txt: _data.resultUrl),
-                  ),
-                );
-              }
-              if (_data.runtimeType == GotinyEntity) {
-                _data = _data as GotinyEntity;
-                return Card(
-                  child: ListTile(
-                    title: const Text('Gotiny'),
-                    subtitle: ShrtcoSmllCard(txt: 'gotiny.cc/${_data.code}'),
-                  ),
-                );
-              }
-              return const Card(
-                margin: EdgeInsets.all(8),
-                child: ListTile(
-                  title: Text('Nah'),
-                  subtitle: Text('Hah 2'),
-                ),
-              )
-                  .animate(
-                    onPlay: (controller) => controller.repeat(),
-                  )
-                  .shimmer(
-                    delay: 500.milliseconds,
-                    angle: 2,
-                    color: Theme.of(context).colorScheme.secondary,
-                    curve: Curves.easeIn,
-                    duration: 800.milliseconds,
-                  );
-            },
-          );
+          return ShortUrlCardListView(state: state);
         }
         if (state is ViewshorturlError) {
           return Center(
