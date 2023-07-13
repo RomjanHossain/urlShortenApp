81b144ebce12e8ed52ea4a703519b4e2f9b1cd8a
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-13 15:42:19 +0600

    urlvis premium

--- lib/data/repositories/ulvis_repo_impl.dart
+++ lib/data/repositories/ulvis_repo_impl.dart
@@ -4,7 +4,7 @@ import 'package:url_shorten/core/error/api_errors.dart';
 import 'package:url_shorten/core/resources/free_resources.dart';
 import 'package:url_shorten/domain/entities/ulvis_entities.dart';
 import 'package:http/http.dart' as http;
-import 'package:url_shorten/domain/repositories/ulvis_entities.dart';
+import 'package:url_shorten/domain/repositories/ulvis_repo.dart';
 
 class UlvisRepoImpl extends UlvisRepository {
   @override
@@ -12,6 +12,27 @@ class UlvisRepoImpl extends UlvisRepository {
     const String ulvisUrl = FreeApiResources.ulVis;
     try {
       final Uri uri = Uri.parse('$ulvisUrl?url=$url');
+      final http.Response response = await http.get(uri);
+      // check if the response is successful
+      if (response.statusCode == 200) {
+        final result = json.decode(response.body);
+        final shrtcoEntities = UlvisEntity.fromJson(result);
+        return Success(shrtcoEntities);
+      } else {
+        // print('oh no! 400 -> ${response.statusCode}\n${response.body}');
+        return ServerFailor(Exception('Unable to get short url'));
+      }
+    } on Exception catch (e) {
+      return ServerFailor(e);
+    }
+  }
+
+  @override
+  Future<Result<UlvisEntity, Exception>> prShortUrl(
+      String url, String custom) async {
+    const String ulvisUrl = FreeApiResources.ulVis;
+    try {
+      final Uri uri = Uri.parse('$ulvisUrl?url=$url&custom=$custom');
       final http.Response response = await http.get(uri);
       // check if the response is successful
       if (response.statusCode == 200) {
