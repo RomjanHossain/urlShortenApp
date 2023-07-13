333b1ea2d6a0dbac23fa872fdafe56a5b2820311
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-13 15:44:04 +0600

    urlbae repo

---
+++ lib/domain/repositories/urlbae_repo.dart
@@ -0,0 +1,7 @@
+import 'package:url_shorten/core/error/api_errors.dart';
+import 'package:url_shorten/domain/entities/urlbae_entities.dart';
+
+/// ShortUrlRepository
+abstract class UrlBaeRepository {
+  Future<Result<UrlBaeEntity, Exception>> shortUrl(String url, String custom);
+}
