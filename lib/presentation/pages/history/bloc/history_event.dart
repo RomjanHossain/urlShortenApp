f416b6e6a16f70026319d085845b94b39d29b2e6
RomjanHossain <romjan@skiff.com>
2023-07-14 18:36:44 +0600

    history event

--- lib/presentation/pages/history/bloc/history_event.dart
+++ lib/presentation/pages/history/bloc/history_event.dart
@@ -1,17 +1,18 @@
 part of 'history_bloc.dart';
 
-abstract class HistoryEvent  extends Equatable {
+abstract class HistoryEvent extends Equatable {
   const HistoryEvent();
 
   @override
   List<Object> get props => [];
-
 }
 
 /// {@template custom_history_event}
 /// Event added when some custom logic happens
 /// {@endtemplate}
-class CustomHistoryEvent extends HistoryEvent {
+class ChangeSegmentBtnE extends HistoryEvent {
   /// {@macro custom_history_event}
-  const CustomHistoryEvent();
+  const ChangeSegmentBtnE(this.segmentButtonEnum);
+
+  final HistorySegmentButtonEnum segmentButtonEnum;
 }
