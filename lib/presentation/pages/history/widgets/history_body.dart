643e1a8229b4d5ee6abc43a120b41cba0ef83ede
RomjanHossain <romjan@skiff.com>
2023-07-14 18:36:01 +0600

    history body

--- lib/presentation/pages/history/widgets/history_body.dart
+++ lib/presentation/pages/history/widgets/history_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_shorten/core/params/history_page_params.dart';
 import 'package:url_shorten/presentation/pages/history/bloc/bloc.dart';
+import 'package:url_shorten/presentation/pages/home/bloc/bloc.dart';
 
 /// {@template history_body}
 /// Body of the HistoryPage.
@@ -12,10 +14,40 @@ class HistoryBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<HistoryBloc, HistoryState>(
-      builder: (context, state) {
-        return Center(child: Text(state.customProperty));
-      },
+    return Column(
+      children: [
+        /// segment button
+        Center(
+          child: BlocConsumer<HistoryBloc, HistoryState>(
+            listener: (context, state) {},
+            builder: (context, state) {
+              if (state is HistoryInitial) {
+                return SegmentedButton(
+                    segments: const <ButtonSegment<HistorySegmentButtonEnum>>[
+                      ButtonSegment(
+                        label: Text('Short URL'),
+                        value: HistorySegmentButtonEnum.shorturl,
+                      ),
+                      ButtonSegment(
+                        label: Text('Alias URL'),
+                        value: HistorySegmentButtonEnum.aliasurl,
+                      ),
+                    ],
+                    selected: <HistorySegmentButtonEnum>{
+                      state.segmentButtonEnum
+                    },
+                    onSelectionChanged:
+                        (Set<HistorySegmentButtonEnum> newSelection) {
+                      context
+                          .read<HistoryBloc>()
+                          .add(ChangeSegmentBtnE(newSelection.first));
+                    });
+              }
+              return const SizedBox();
+            },
+          ),
+        ),
+      ],
     );
   }
 }
