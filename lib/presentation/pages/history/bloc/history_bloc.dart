1c6ddfcdbc454faab668701682897bdd64e3efaa
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-14 18:34:44 +0600

    history bloc

--- lib/presentation/pages/history/bloc/history_bloc.dart
+++ lib/presentation/pages/history/bloc/history_bloc.dart
@@ -2,18 +2,21 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:url_shorten/core/params/history_page_params.dart';
 part 'history_event.dart';
 part 'history_state.dart';
 
 class HistoryBloc extends Bloc<HistoryEvent, HistoryState> {
-  HistoryBloc() : super(const HistoryInitial()) {
-    on<CustomHistoryEvent>(_onCustomHistoryEvent);
+  HistoryBloc()
+      : super(HistoryInitial(
+            segmentButtonEnum: HistorySegmentButtonEnum.shorturl)) {
+    on<ChangeSegmentBtnE>(_onCustomHistoryEvent);
   }
 
   FutureOr<void> _onCustomHistoryEvent(
-    CustomHistoryEvent event,
+    ChangeSegmentBtnE event,
     Emitter<HistoryState> emit,
   ) {
-    // TODO: Add Logic
+    emit(HistoryInitial(segmentButtonEnum: event.segmentButtonEnum));
   }
 }
