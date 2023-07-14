23cca062ebe9af88273e78237e50e51e550c01e2
RomjanHossain <romjan@skiff.com>
2023-07-15 00:07:20 +0600

    history bloc and state

--- lib/presentation/pages/history/bloc/history_state.dart
+++ lib/presentation/pages/history/bloc/history_state.dart
@@ -11,10 +11,23 @@ class HistoryState extends Equatable {
 /// The initial state of HistoryState
 /// {@endtemplate}
 class HistoryInitial extends HistoryState {
-  HistoryInitial({required this.segmentButtonEnum}) : super();
+  HistoryInitial({
+    required this.segmentButtonEnum,
+    required this.shortUrlsFree,
+    required this.shrtCoUrlFree,
+  }) : super();
 
   final HistorySegmentButtonEnum segmentButtonEnum;
 
+  /// list of short urls (from local DB)
+  final List<ShortUrlContainerDBModel> shortUrlsFree;
+
+  /// ShrtcoDBModel
+  final List<ShrtcoDBModel> shrtCoUrlFree;
+
   @override
-  List<Object> get props => [segmentButtonEnum];
+  List<Object> get props => [
+        segmentButtonEnum,
+        shortUrlsFree,
+      ];
 }
