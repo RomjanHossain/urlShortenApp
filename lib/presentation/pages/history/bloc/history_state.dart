6b50e051ba758dcf30f54323be33141a5f9c796f
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-14 18:35:52 +0600

    history states

--- lib/presentation/pages/history/bloc/history_state.dart
+++ lib/presentation/pages/history/bloc/history_state.dart
@@ -1,34 +1,20 @@
 part of 'history_bloc.dart';
 
-/// {@template history_state}
 /// HistoryState description
-/// {@endtemplate}
+
 class HistoryState extends Equatable {
-  /// {@macro history_state}
-  const HistoryState({
-    this.customProperty = 'History Page',
-  });
-
-  /// A description for customProperty
-  final String customProperty;
-
   @override
-  List<Object> get props => [customProperty];
-
-  /// Creates a copy of the current HistoryState with property changes
-  HistoryState copyWith({
-    String? customProperty,
-  }) {
-    return HistoryState(
-      customProperty: customProperty ?? this.customProperty,
-    );
-  }
+  List<Object> get props => [];
 }
 
 /// {@template history_initial}
 /// The initial state of HistoryState
 /// {@endtemplate}
 class HistoryInitial extends HistoryState {
-  /// {@macro history_initial}
-  const HistoryInitial() : super();
+  HistoryInitial({required this.segmentButtonEnum}) : super();
+
+  final HistorySegmentButtonEnum segmentButtonEnum;
+
+  @override
+  List<Object> get props => [segmentButtonEnum];
 }
