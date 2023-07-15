0167e68f4ae61814363c778c6f6cdc491a92af4f
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-16 00:20:54 +0600

    remove the history event

--- lib/presentation/pages/splash/widgets/splash_body.dart
+++ lib/presentation/pages/splash/widgets/splash_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_shorten/presentation/pages/history/bloc/bloc.dart';
 import 'package:url_shorten/presentation/pages/splash/bloc/bloc.dart';
 
 /// {@template splash_body}
@@ -14,6 +15,7 @@ class SplashBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<SplashBloc, SplashState>(
       builder: (context, state) {
+        // context.read<HistoryBloc>().add(HistoryInitial(segmentButtonEnum: segmentButtonEnum, shortUrlsFree: shortUrlsFree, shortUrlsPR: shortUrlsPR, shrtCoUrlFree: shrtCoUrlFree))
         return const Center(child: CircularProgressIndicator());
       },
     );
