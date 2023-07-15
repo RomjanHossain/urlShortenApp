250d9657157b47c6a691250af7f7d33fa91d6158
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-16 00:25:28 +0600

    history card list view

---
+++ lib/presentation/pages/history/components/history_card_list_view.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:url_shorten/data/models/shorturl_container_db_model.dart';
+import 'package:url_shorten/presentation/pages/history/components/history_card_link_comp.dart';
+
+class HistoryCardListTile extends StatelessWidget {
+  const HistoryCardListTile({
+    super.key,
+    required this.shortUrlModel,
+  });
+
+  final ShortUrlContainerDBModel shortUrlModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        shortUrlModel.domain ?? 'x',
+      ),
+      subtitle: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          HistoryCardLinkText(
+            linkType: 'Short Link',
+            shortLink: shortUrlModel.shortLink ?? '',
+          ),
+          HistoryCardLinkText(
+            linkType: 'Originnal Link',
+            shortLink: shortUrlModel.originalLink ?? '',
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text('Created at: '),
+              // Text(shortUrlModel.creationDate.toString()),
+              Text(DateFormat('yyyy-MM-dd – kk:mm')
+                  .format(shortUrlModel.creationDate)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
