f1dc8431642f99b59fbdf211d680fe3b5630d44e
RomjanHossain <romjan@skiff.com>
2023-07-16 00:24:41 +0600

    home nav

---
+++ lib/presentation/pages/home/components/home_navbar.dart
@@ -0,0 +1,51 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:url_shorten/core/params/history_page_params.dart';
+import 'package:url_shorten/core/params/home_page_params.dart';
+import 'package:url_shorten/presentation/pages/home/bloc/home_bloc.dart';
+
+import '../../history/bloc/history_bloc.dart';
+
+/// home page bottom navigation bar
+class HomeBottomNav extends StatelessWidget {
+  const HomeBottomNav({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    HomeState state = context.watch<HomeBloc>().state;
+    return AnimatedBottomNavigationBar(
+      activeIndex: state is HomeInitial
+          ? 0
+          : state is FavoritePageState
+              ? 1
+              : state is HistoryPageState
+                  ? 2
+                  : 3,
+      gapLocation: GapLocation.center,
+      notchSmoothness: NotchSmoothness.verySmoothEdge,
+      leftCornerRadius: HomePageParams.bottomNavCornerRaidus,
+      rightCornerRadius: HomePageParams.bottomNavCornerRaidus,
+      icons: const [
+        Icons.home_rounded,
+        Icons.favorite_rounded,
+        Icons.history_rounded,
+        Icons.settings_rounded,
+      ],
+      onTap: (x) {
+        if (x == 2) {
+          context
+              .read<HistoryBloc>()
+              .add(const ChangeSegmentBtnE(HistorySegmentButtonEnum.shorturl));
+        }
+        context.read<HomeBloc>().add(ChangePageEvent(x));
+      },
+      backgroundColor: colorScheme.primaryContainer,
+      activeColor: colorScheme.tertiary,
+      inactiveColor: colorScheme.onBackground,
+    );
+  }
+}
