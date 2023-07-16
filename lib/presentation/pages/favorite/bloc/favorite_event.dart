936777e6f2a0d88cef512dca87670102d9d75145
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-16 23:59:20 +0600

    favorite page bloc, event and states

--- lib/presentation/pages/favorite/bloc/favorite_event.dart
+++ lib/presentation/pages/favorite/bloc/favorite_event.dart
@@ -1,17 +1,39 @@
 part of 'favorite_bloc.dart';
 
-abstract class FavoriteEvent  extends Equatable {
+abstract class FavoriteEvent extends Equatable {
   const FavoriteEvent();
 
   @override
   List<Object> get props => [];
-
 }
 
-/// {@template custom_favorite_event}
 /// Event added when some custom logic happens
-/// {@endtemplate}
 class CustomFavoriteEvent extends FavoriteEvent {
-  /// {@macro custom_favorite_event}
   const CustomFavoriteEvent();
+}
+
+/// ADD TO FAV
+class AddToFavE extends FavoriteEvent {
+  const AddToFavE({required this.shortUrlFavContainerDBModel});
+  final ShortUrlFavContainerDBModel shortUrlFavContainerDBModel;
+}
+
+/// ADD TO FAV
+class AddToShrtFavE extends FavoriteEvent {
+  const AddToShrtFavE({required this.shortUrlFavContainerDBModel});
+  final ShrtcoFavDBModel shortUrlFavContainerDBModel;
+}
+
+/// remove TO FAV
+
+class RemoveFromFavE extends FavoriteEvent {
+  const RemoveFromFavE({required this.id});
+  final int id;
+}
+
+/// remove TO FAV
+
+class RemoveShrtFromFavE extends FavoriteEvent {
+  const RemoveShrtFromFavE({required this.id});
+  final int id;
 }
