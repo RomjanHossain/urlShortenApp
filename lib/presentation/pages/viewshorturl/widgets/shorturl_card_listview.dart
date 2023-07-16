ea8423c4d737a906cb3842aa8354a411280cf8b8
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-17 00:00:35 +0600

    unnessary import

--- lib/presentation/pages/viewshorturl/widgets/shorturl_card_listview.dart
+++ lib/presentation/pages/viewshorturl/widgets/shorturl_card_listview.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:url_shorten/domain/entities/cleanuri_entities.dart';
 import 'package:url_shorten/domain/entities/error_entities.dart';
 import 'package:url_shorten/domain/entities/gotiny_entities.dart';
 import 'package:url_shorten/domain/entities/shrtco_entities.dart';
 import 'package:url_shorten/domain/entities/ulvis_entities.dart';
+import 'package:url_shorten/presentation/pages/favorite/bloc/bloc.dart';
 import 'package:url_shorten/presentation/pages/home/bloc/home_bloc.dart';
 import 'package:url_shorten/presentation/pages/viewshorturl/bloc/viewshorturl_bloc.dart';
+import 'package:url_shorten/presentation/pages/viewshorturl/components/fav_button.dart';
+import 'package:url_shorten/presentation/pages/viewshorturl/components/fav_button2.dart';
 import 'package:url_shorten/presentation/pages/viewshorturl/components/shrt_card.dart';
 import 'package:url_shorten/presentation/pages/viewshorturl/widgets/shrt_small_card.dart';
 
@@ -21,95 +23,144 @@ class ShortUrlCardListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: state.furls.length + 1,
-      itemBuilder: (context, index) {
-        if (index == state.furls.length) {
+    return ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+      child: ListView.builder(
+        itemCount: state.furls.length + 1,
+        itemBuilder: (context, index) {
           var homeState = context.read<HomeBloc>().state;
-          return Column(
-            children: [
-              const Divider(
-                endIndent: 20,
-                indent: 20,
-              ),
-              Card(
-                child: ListTile(
-                  title: const Text('Original Link'),
-                  subtitle: ShrtcoSmllCard(
-                    txt: homeState is HomeInitial
-                        ? homeState.urlController.text
-                        : '',
+          if (index == state.furls.length) {
+            return Column(
+              children: [
+                const Divider(
+                  endIndent: 20,
+                  indent: 20,
+                ),
+                Card(
+                  child: ListTile(
+                    title: const Text('Original Link'),
+                    subtitle: ShrtcoSmllCard(
+                      txt: homeState is HomeInitial
+                          ? homeState.urlController.text
+                          : '',
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 100),
-            ],
-          );
-        }
-        Object data = state.furls[index];
-        if (data.runtimeType == ErrorModel) {
-          data = data as ErrorModel;
-
-          return Card(
-            child: ListTile(
-              title: Text('Errors ${data.domain}'),
-              subtitle: ShrtcoSmllCard(txt: data.message),
-            ),
-          );
-        }
-        if (data.runtimeType == ShrtcoEntity) {
-          data = data as ShrtcoEntity;
-          return ShrtCoCard(
-            shrtcoEntity: data,
-          );
-        }
-        if (data.runtimeType == CleanUriEntities) {
-          data = data as CleanUriEntities;
-          return Card(
-            child: ListTile(
-              title: const Text('CleanUri'),
-              subtitle: ShrtcoSmllCard(
-                txt: data.resultUrl,
-              ),
-            ),
-          );
-        }
-        if (data.runtimeType == GotinyEntity) {
-          data = data as GotinyEntity;
-          return Card(
-            child: ListTile(
-              title: const Text('Gotiny'),
-              subtitle: ShrtcoSmllCard(txt: 'gotiny.cc/${data.code}'),
-            ),
-          );
-        }
-        if (data.runtimeType == UlvisEntity) {
-          data = data as UlvisEntity;
-          return Card(
-            child: ListTile(
-              title: const Text('Ulvis'),
-              subtitle: ShrtcoSmllCard(txt: data.data.url),
-            ),
-          );
-        }
-        return const Card(
-          margin: EdgeInsets.all(8),
-          child: ListTile(
-            title: Text('Nah'),
-            subtitle: Text('Hah 2'),
-          ),
-        )
-            .animate(
-              onPlay: (controller) => controller.repeat(),
-            )
-            .shimmer(
-              delay: 500.milliseconds,
-              angle: 2,
-              color: Theme.of(context).colorScheme.secondary,
-              curve: Curves.easeIn,
-              duration: 800.milliseconds,
+                const SizedBox(height: 100),
+              ],
             );
-      },
+          }
+          Object data = state.furls[index];
+          if (data.runtimeType == ErrorModel) {
+            data = data as ErrorModel;
+
+            return Card(
+              child: ListTile(
+                title: Text('Errors ${data.domain}'),
+                subtitle: ShrtcoSmllCard(txt: data.message),
+              ),
+            );
+          }
+          if (data.runtimeType == ShrtcoEntity) {
+            data = data as ShrtcoEntity;
+            return Stack(
+              children: [
+                ShrtCoCard(
+                  shrtcoEntity: data,
+                ),
+                IsFavIconButton2(
+                  url1: data.shortLink,
+                  url2: data.shortLink2,
+                  url3: data.shortLink3,
+                  domain: 'ShrtCo',
+                  originURL: homeState is HomeInitial
+                      ? homeState.urlController.text
+                      : '',
+                ),
+              ],
+            );
+          }
+          if (data.runtimeType == CleanUriEntities) {
+            data = data as CleanUriEntities;
+            return Stack(
+              children: [
+                Card(
+                  child: ListTile(
+                    title: const Text('CleanUri'),
+                    subtitle: ShrtcoSmllCard(
+                      txt: data.resultUrl,
+                    ),
+                  ),
+                ),
+                IsFavIconButton(
+                  url: data.resultUrl,
+                  domain: 'CleanUri',
+                  originURL: homeState is HomeInitial
+                      ? homeState.urlController.text
+                      : '',
+                ),
+              ],
+            );
+          }
+          if (data.runtimeType == GotinyEntity) {
+            data = data as GotinyEntity;
+            return Stack(
+              children: [
+                Card(
+                  child: ListTile(
+                    title: const Text('Gotiny'),
+                    subtitle: ShrtcoSmllCard(txt: 'gotiny.cc/${data.code}'),
+                  ),
+                ),
+                IsFavIconButton(
+                  url: 'gotiny.cc/${data.code}',
+                  domain: 'Gotiny',
+                  originURL: homeState is HomeInitial
+                      ? homeState.urlController.text
+                      : '',
+                ),
+              ],
+            );
+          }
+          if (data.runtimeType == UlvisEntity) {
+            data = data as UlvisEntity;
+            return Stack(
+              children: [
+                Card(
+                  child: ListTile(
+                    title: const Text('Ulvis'),
+                    subtitle: ShrtcoSmllCard(txt: data.data.url),
+                  ),
+                ),
+                IsFavIconButton(
+                  url: data.data.url,
+                  domain: 'Ulvis',
+                  originURL: homeState is HomeInitial
+                      ? homeState.urlController.text
+                      : '',
+                ),
+              ],
+            );
+          }
+          return const Card(
+            margin: EdgeInsets.all(8),
+            child: ListTile(
+              title: Text('Nah'),
+              subtitle: Text('Hah 2'),
+            ),
+          )
+              .animate(
+                onPlay: (controller) => controller.repeat(),
+              )
+              .shimmer(
+                delay: 500.milliseconds,
+                angle: 2,
+                color: Theme.of(context).colorScheme.secondary,
+                curve: Curves.easeIn,
+                duration: 800.milliseconds,
+              );
+        },
+      ),
     );
   }
 }
--- lib/presentation/pages/viewshorturlpr/widgets/short_url_card_list_view_pr.dart
+++ lib/presentation/pages/viewshorturlpr/widgets/short_url_card_list_view_pr.dart
@@ -18,83 +18,86 @@ class ShortUrlCardListViewPr extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: state.surls.length + 1,
-      itemBuilder: (context, index) {
-        if (index == state.surls.length) {
-          return const SizedBox(height: 100);
-        }
-        Object data = state.surls[index];
+    return ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+      child: ListView.builder(
+        itemCount: state.surls.length + 1,
+        itemBuilder: (context, index) {
+          if (index == state.surls.length) {
+            return const SizedBox(height: 100);
+          }
+          Object data = state.surls[index];
 
-        print('$index ${data.runtimeType}');
-        if (data.runtimeType == UlvisEntity) {
-          data = data as UlvisEntity;
-          return Card(
-            child: ListTile(
-              title: const Text('Ulvis'),
-              subtitle: ShrtcoSmllCard(txt: data.data.url),
-            ),
-          );
-        }
-        if (data.runtimeType == RebrandlyEntities) {
-          data = data as RebrandlyEntities;
-          return Card(
-            child: ListTile(
-              title: const Text('Rebrandly'),
-              subtitle: ShrtcoSmllCard(txt: data.shortURL),
-            ),
-          );
-        }
-        if (data.runtimeType == TinyUrlEntities) {
-          data = data as TinyUrlEntities;
-
-          return Card(
-            child: ListTile(
-              title: const Text('TinyUrl'),
-              subtitle: ShrtcoSmllCard(txt: data.data?.tinyUrl ?? ""),
-            ),
-          );
-        }
-        if (data.runtimeType == ErrorModel) {
-          data = data as ErrorModel;
-
-          return Card(
-            child: ListTile(
-              title: Text('Errors ${data.domain}'),
-              subtitle: ShrtcoSmllCard(txt: data.message),
-            ),
-          );
-        }
-        if (data.runtimeType == UrlBaeEntity) {
-          data = data as UrlBaeEntity;
-// ErrorModel
-          return Card(
-            child: ListTile(
-              title: const Text('UrlBae'),
-              subtitle: ShrtcoSmllCard(
-                txt: data.shortUrl ?? 'Empty',
+          print('$index ${data.runtimeType}');
+          if (data.runtimeType == UlvisEntity) {
+            data = data as UlvisEntity;
+            return Card(
+              child: ListTile(
+                title: const Text('Ulvis'),
+                subtitle: ShrtcoSmllCard(txt: data.data.url),
               ),
-            ),
-          );
-        }
-        return const Card(
-          margin: EdgeInsets.all(8),
-          child: ListTile(
-            title: Text('Nah'),
-            subtitle: Text('Hah 2'),
-          ),
-        )
-            .animate(
-              onPlay: (controller) => controller.repeat(),
-            )
-            .shimmer(
-              delay: 500.milliseconds,
-              angle: 2,
-              color: Theme.of(context).colorScheme.secondary,
-              curve: Curves.easeIn,
-              duration: 800.milliseconds,
             );
-      },
+          }
+          if (data.runtimeType == RebrandlyEntities) {
+            data = data as RebrandlyEntities;
+            return Card(
+              child: ListTile(
+                title: const Text('Rebrandly'),
+                subtitle: ShrtcoSmllCard(txt: data.shortURL),
+              ),
+            );
+          }
+          if (data.runtimeType == TinyUrlEntities) {
+            data = data as TinyUrlEntities;
+
+            return Card(
+              child: ListTile(
+                title: const Text('TinyUrl'),
+                subtitle: ShrtcoSmllCard(txt: data.data?.tinyUrl ?? ""),
+              ),
+            );
+          }
+          if (data.runtimeType == ErrorModel) {
+            data = data as ErrorModel;
+
+            return Card(
+              child: ListTile(
+                title: Text('Errors ${data.domain}'),
+                subtitle: ShrtcoSmllCard(txt: data.message),
+              ),
+            );
+          }
+          if (data.runtimeType == UrlBaeEntity) {
+            data = data as UrlBaeEntity;
+            // ErrorModel
+            return Card(
+              child: ListTile(
+                title: const Text('UrlBae'),
+                subtitle: ShrtcoSmllCard(
+                  txt: data.shortUrl ?? 'Empty',
+                ),
+              ),
+            );
+          }
+          return const Card(
+            margin: EdgeInsets.all(8),
+            child: ListTile(
+              title: Text('Nah'),
+              subtitle: Text('Hah 2'),
+            ),
+          )
+              .animate(
+                onPlay: (controller) => controller.repeat(),
+              )
+              .shimmer(
+                delay: 500.milliseconds,
+                angle: 2,
+                color: Theme.of(context).colorScheme.secondary,
+                curve: Curves.easeIn,
+                duration: 800.milliseconds,
+              );
+        },
+      ),
     );
   }
 }
