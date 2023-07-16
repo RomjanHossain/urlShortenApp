78bca2ae182b850f9bfbbefac56bf175cfb69d7a
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-17 00:01:51 +0600

    stupid import

--- lib/presentation/pages/splash/widgets/splash_body.dart
+++ lib/presentation/pages/splash/widgets/splash_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_shorten/presentation/pages/history/bloc/bloc.dart';
 import 'package:url_shorten/presentation/pages/splash/bloc/bloc.dart';
 
 /// {@template splash_body}
