b5ecce0e83722df6fd42b3884a3228e67d7035dd
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-17 00:00:50 +0600

    fav btn

---
+++ lib/presentation/pages/viewshorturl/components/fav_button.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:url_shorten/data/models/shorturl_container_fav_db_model.dart';
+import 'package:url_shorten/presentation/pages/favorite/bloc/favorite_bloc.dart';
+
+class IsFavIconButton extends StatelessWidget {
+  const IsFavIconButton({
+    super.key,
+    required this.url,
+    required this.domain,
+    required this.originURL,
+  });
+  final String url;
+  final String domain;
+  final String originURL;
+  // final IsFavoriteUseCases _isFav = IsFavoriteUseCases();
+
+  @override
+  Widget build(BuildContext context) {
+    // final favState = context.read<FavoriteBloc>().state;
+
+    return BlocConsumer<FavoriteBloc, FavoriteState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          if (state is FavoriteInitial) {
+            final isFav = state.shortUrlsFree.any((element) {
+              return element.shortLink == url;
+            });
+            return Align(
+              alignment: Alignment.topRight,
+              child: IconButton.outlined(
+                onPressed: () {
+                  // state.shortUrlsFree.forEach((e) => print(e.shortLink));
+
+                  if (isFav) {
+                    print('oh yeah! this is fav');
+                    ShortUrlFavContainerDBModel d = state.shortUrlsFree
+                        .where((element) => element.shortLink == url)
+                        .first;
+                    print(d.shortLink);
+                    context.read<FavoriteBloc>().add(
+                          RemoveFromFavE(
+                            id: d.id,
+                          ),
+                        );
+                  } else {
+                    print('nah man! this aint');
+
+                    context.read<FavoriteBloc>().add(AddToFavE(
+                        shortUrlFavContainerDBModel:
+                            ShortUrlFavContainerDBModel()
+                              ..domain = domain
+                              ..originalLink = originURL
+                              ..shortLink = url));
+                  }
+                },
+                icon: isFav
+                    ? const Icon(Icons.favorite_rounded)
+                    : const Icon(Icons.favorite_border_rounded),
+              ),
+            );
+          }
+          return SizedBox();
+        });
+
+    // return const SizedBox.shrink();
+  }
+}
