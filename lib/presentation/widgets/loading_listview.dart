974a257fd5ddc7732ac18e137fb5793b000fffb8
RomjanHossain <romjan@skiff.com>
2023-07-17 00:00:35 +0600

    unnessary import

--- lib/presentation/widgets/loading_listview.dart
+++ lib/presentation/widgets/loading_listview.dart
@@ -8,30 +8,33 @@ class LoadingListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 4,
-      itemBuilder: (context, index) {
-        return SizedBox(
-          height: 200,
-          child: const Card(
-            margin: EdgeInsets.all(8),
-            child: ListTile(
-              title: Text(''),
-              subtitle: Text(''),
-            ),
-          )
-              .animate(
-                onPlay: (controller) => controller.repeat(),
-              )
-              .shimmer(
-                delay: 500.milliseconds,
-                angle: 2,
-                color: Theme.of(context).colorScheme.secondary,
-                curve: Curves.easeIn,
-                duration: 800.milliseconds,
+    return ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+      child: ListView.builder(
+        itemCount: 4,
+        itemBuilder: (context, index) {
+          return SizedBox(
+            height: 200,
+            child: const Card(
+              margin: EdgeInsets.all(8),
+              child: ListTile(
+                title: Text(''),
+                subtitle: Text(''),
               ),
-        );
-      },
+            )
+                .animate(
+                  onPlay: (controller) => controller.repeat(),
+                )
+                .shimmer(
+                  delay: 500.milliseconds,
+                  angle: 2,
+                  color: Theme.of(context).colorScheme.secondary,
+                  curve: Curves.easeIn,
+                  duration: 800.milliseconds,
+                ),
+          );
+        },
+      ),
     );
   }
 }
