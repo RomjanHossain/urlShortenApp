e4dc6dfbab005dff1ffd1edb99fe34c7e239663f
RomjanHossain <romjan@skiff.com>
2023-07-17 00:02:22 +0600

    favorite body

--- lib/presentation/pages/favorite/widgets/favorite_body.dart
+++ lib/presentation/pages/favorite/widgets/favorite_body.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:url_shorten/presentation/pages/favorite/bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:url_shorten/data/models/shorturl_container_fav_db_model.dart';
+import 'package:url_shorten/data/models/shrtco_fav_db_model.dart';
+import 'package:url_shorten/presentation/pages/favorite/bloc/favorite_bloc.dart';
+import 'package:url_shorten/presentation/pages/favorite/components/fav_card_comp.dart';
+import 'package:url_shorten/presentation/pages/favorite/components/fav_card_shrtco_comp.dart';
 
 /// {@template favorite_body}
 /// Body of the FavoritePage.
@@ -14,6 +19,25 @@ class FavoriteBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<FavoriteBloc, FavoriteState>(
       builder: (context, state) {
+        if (state is FavoriteInitial) {
+          return ScrollConfiguration(
+            behavior:
+                ScrollConfiguration.of(context).copyWith(scrollbars: false),
+            child: ListView(
+              children: [
+                for (ShrtcoFavDBModel shrtUrlModel in state.shrtCoUrlFree)
+                  FavShrtCoCard(
+                    shrtCoUrlModel: shrtUrlModel,
+                  ),
+                for (ShortUrlFavContainerDBModel shortUrlModel
+                    in state.shortUrlsFree)
+                  FavCard(
+                    shortUrlModel: shortUrlModel,
+                  ),
+              ],
+            ),
+          );
+        }
         return Center(child: Text(state.customProperty));
       },
     );
