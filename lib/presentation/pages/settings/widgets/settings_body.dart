b65918b75e6e2a081ae57b249b270eea61dcb0e7
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-19 23:34:45 +0600

    settings body in DEMO

--- lib/presentation/pages/settings/widgets/settings_body.dart
+++ lib/presentation/pages/settings/widgets/settings_body.dart
@@ -14,7 +14,43 @@ class SettingsBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<SettingsBloc, SettingsState>(
       builder: (context, state) {
-        return Center(child: Text(state.customProperty));
+        return ListView(
+          children: [
+            // app logo
+            FlutterLogo(
+              size: 125,
+            ),
+            // app name
+            Center(
+              child: Text(
+                'Url Shorten',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+            // view repo button
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('View Repo'),
+            ),
+            // view authors profile
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('View Authors Profile'),
+            ),
+
+            /// delete all history button
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Delete All History'),
+            ),
+
+            /// delete all favorite
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Delete All Favorite'),
+            ),
+          ],
+        );
       },
     );
   }
