b5aa0d8d6544bff5729503a26e017283c3496502
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-20 23:26:58 +0600

    adding the all delte event

--- lib/presentation/pages/settings/bloc/settings_bloc.dart
+++ lib/presentation/pages/settings/bloc/settings_bloc.dart
@@ -2,12 +2,16 @@ import 'dart:async';
 
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:url_shorten/data/datasources/local/shorturl_db_impl.dart';
 part 'settings_event.dart';
 part 'settings_state.dart';
 
 class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
+  final ShortDBImplementation _shortDBImplementation = ShortDBImplementation();
   SettingsBloc() : super(const SettingsInitial()) {
     on<CustomSettingsEvent>(_onCustomSettingsEvent);
+    on<DeleteAllHistoryE>(_onDeleteAllHistory);
+    on<DeleteAllFavE>(_onDeleteAllFav);
   }
 
   FutureOr<void> _onCustomSettingsEvent(
@@ -15,5 +19,23 @@ class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
     Emitter<SettingsState> emit,
   ) {
     // TODO: Add Logic
+  }
+
+  /// delete all history
+  FutureOr<void> _onDeleteAllHistory(
+    DeleteAllHistoryE event,
+    Emitter<SettingsState> emit,
+  ) async {
+    await _shortDBImplementation.removeAllHistory();
+    // emit(const DeleteAllHistoryS());
+  }
+
+  /// delete all fav
+  FutureOr<void> _onDeleteAllFav(
+    DeleteAllFavE event,
+    Emitter<SettingsState> emit,
+  ) async {
+    await _shortDBImplementation.removeAllFav();
+    // emit(const DeleteAllFavS());
   }
 }
