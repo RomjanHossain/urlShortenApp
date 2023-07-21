4599add38afd8092e4376410ba374d410232e08d
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-21 18:34:18 +0600

    repo and author url

---
+++ lib/core/params/settings_page_params.dart
@@ -0,0 +1,10 @@
+enum DeleteType {
+  deleteAllHistory,
+  deleteAllFav,
+}
+
+class SettingPageParams {
+  static const String viewRepo =
+      'https://github.com/RomjanHossain/urlShortenApp';
+  static const String viewAuthor = 'https://github.com/RomjanHossain/';
+}
