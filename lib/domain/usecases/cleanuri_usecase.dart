0e0166ea00074599e3192acc3ea6e0525b093fc9
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-22 07:58:31 +0600

    unnecessay await

--- lib/domain/usecases/cleanuri_usecase.dart
+++ lib/domain/usecases/cleanuri_usecase.dart
@@ -6,11 +6,12 @@ class CleanUriUseCase {
   CleanUriRepoImpl repository = CleanUriRepoImpl();
 
   /// get shorted url or erros
+  // ignore: prefer_expression_function_bodies
   Future<Result<CleanUriEntities, Exception>> getShortUrl(String url) async {
     /*
     !!!If theres any business logic that needs to be done 
     !!!before returning the data, it should be done here
     */
-    return await repository.shortUrl(url);
+    return repository.shortUrl(url);
   }
 }
