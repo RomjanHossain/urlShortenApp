2eae60bb225d8ef9e7b5360bd0a8337a6fd9219e
RomjanHossain <romjan@skiff.com>
2023-07-22 07:11:12 +0600

    app colorscheme

--- lib/core/params/home_page_params.dart
+++ lib/core/params/home_page_params.dart
@@ -1,26 +1,29 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
+// ignore: avoid_classes_with_only_static_members
 class HomePageParams {
   /// Bottom Navigation radius
   static const double bottomNavCornerRaidus = 15;
 
   /// dark colorshceme
   static ThemeData darkTheme = FlexThemeData.dark(
-    scheme: FlexScheme.orangeM3,
+    scheme: FlexScheme.pinkM3,
+    primary: const Color(0xffbe63f9),
     useMaterial3: true,
   );
 
   /// light colorshceme
   static ThemeData lightTheme = FlexThemeData.light(
-    scheme: FlexScheme.orangeM3,
+    scheme: FlexScheme.pinkM3,
+    primary: const Color(0xffbe63f9),
     useMaterial3: true,
   );
   //To check if a string is a valid URL
   static bool isURL(String url) {
     // Regular expression pattern to match a URL
-    const pattern = r'^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$';
-    final regex = RegExp(pattern);
+    const String pattern = r'^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$';
+    final RegExp regex = RegExp(pattern);
     return regex.hasMatch(url);
   }
 }
