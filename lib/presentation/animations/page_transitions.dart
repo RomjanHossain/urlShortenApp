3f73d4acd47898dadd9b51f7c340d66e502b51a5
RomjanHossain <romjan@skiff.com>
2023-07-22 08:00:15 +0600

    type defined

--- lib/presentation/animations/page_transitions.dart
+++ lib/presentation/animations/page_transitions.dart
@@ -1,39 +1,55 @@
+// ignore_for_file: always_specify_types
+
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 
+// ignore: avoid_classes_with_only_static_members
 /// page animatin class
 class PageAnimationWrapper {
 // fade through transition page wrapper
-  static Route fadeThroughTransitionPageWrapper(Widget nxtPage) {
-    return PageRouteBuilder(
-      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-          FadeThroughTransition(
-        fillColor: Theme.of(context).scaffoldBackgroundColor,
-        animation: animation,
-        secondaryAnimation: secondaryAnimation,
-        child: child,
-      ),
-      pageBuilder: (context, animation, secondaryAnimation) {
-        return nxtPage;
-      },
-    );
-  }
+  static Route fadeThroughTransitionPageWrapper(Widget nxtPage) =>
+      PageRouteBuilder(
+        transitionsBuilder: (
+          BuildContext context,
+          Animation<double> animation,
+          Animation<double> secondaryAnimation,
+          Widget child,
+        ) =>
+            FadeThroughTransition(
+          fillColor: Theme.of(context).scaffoldBackgroundColor,
+          animation: animation,
+          secondaryAnimation: secondaryAnimation,
+          child: child,
+        ),
+        pageBuilder: (
+          BuildContext context,
+          Animation<double> animation,
+          Animation<double> secondaryAnimation,
+        ) =>
+            nxtPage,
+      );
 
 // shared axis transition page wrapper
-  static Route sharedAxisTransitionPageWrapper(Widget nxtPage) {
-    return PageRouteBuilder(
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return SharedAxisTransition(
+  static Route sharedAxisTransitionPageWrapper(Widget nxtPage) =>
+      PageRouteBuilder(
+        transitionsBuilder: (
+          BuildContext context,
+          Animation<double> animation,
+          Animation<double> secondaryAnimation,
+          Widget child,
+        ) =>
+            SharedAxisTransition(
           fillColor: Theme.of(context).cardColor,
           animation: animation,
           secondaryAnimation: secondaryAnimation,
           transitionType: SharedAxisTransitionType.scaled,
           child: child,
-        );
-      },
-      pageBuilder: (context, animation, secondaryAnimation) {
-        return nxtPage;
-      },
-    );
-  }
+        ),
+        pageBuilder: (
+          BuildContext context,
+          Animation<double> animation,
+          Animation<double> secondaryAnimation,
+        ) =>
+            nxtPage,
+      );
 }
