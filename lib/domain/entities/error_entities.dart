b10ea0b1a860de182b0fc58f6cc9c2b6b2495fd1
RomjanHossain <romjan@skiff.com>
2023-07-22 07:56:46 +0600

    constractor before finals

--- lib/domain/entities/error_entities.dart
+++ lib/domain/entities/error_entities.dart
@@ -1,10 +1,10 @@
 /// Error model
 class ErrorModel {
-  final String message;
-  final String domain;
 
   ErrorModel({
     required this.message,
     required this.domain,
   });
+  final String message;
+  final String domain;
 }
